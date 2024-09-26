5429d0a2c8c4b62ec409b1d474d86c878f819ba3
ParanoidAndroid <mirko.menegazzo@gmail.com>
2024-09-26 13:26:53 +0200

    Create 3bmeteo.js

---
+++ 3bmeteo.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Remove Top Padding on Mobile (3bmeteo.com)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Removes top padding on mobile devices only on 3bmeteo.com
+// @author       Your Name
+// @match        https://www.3bmeteo.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if (window.location.href.startsWith("https://www.3bmeteo.com/")) {
+        // Add the CSS rules only on 3bmeteo.com
+        const style = document.createElement('style');
+        style.textContent = `
+            @media (max-width: 767px) {
+                body {
+                    padding-top: 0 !important;
+                }
+            }
+        `;
+        document.head.appendChild(style);
+    }
+})();
